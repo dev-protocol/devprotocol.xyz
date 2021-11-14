dba49d4ccc09b839f74ff1e29e75c9bf94b40088
AbhinavMV <mvabhinav1998@gmail.com>
2021-11-14 13:38:44 +0530

    Initial Test Navbar Design

---
+++ src/components/Global/Navbar.vue
@@ -0,0 +1,346 @@
+<template>
+  <header
+    class="
+      text-black
+      sm:flex sm:justify-between sm:items-center sm:px-4 sm:py-3
+      border-b-2
+    "
+  >
+    <div class="flex items-center justify-between px-4 py-3 sm:p-0 text-3xl">
+      <a href="https://devprotocol.xyz" class="text-gray-900">
+        <img src="/assets/logo.png" alt="DevProtocol" class="max-h-8" />
+      </a>
+      <div class="sm:hidden">
+        <button
+          @click="smIsOpen = !smIsOpen"
+          type="button"
+          class="
+            block
+            text-gray-800
+            hover:text-black
+            focus:text-black focus:outline-none
+          "
+        >
+          <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
+            <path
+              v-if="smIsOpen"
+              fill-rule="evenodd"
+              d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
+            />
+            <path
+              v-if="!smIsOpen"
+              fill-rule="evenodd"
+              d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+    <nav
+      :class="smIsOpen ? 'block border' : 'hidden'"
+      class="px-2 pt-2 pb-4 sm:flex sm:p-0"
+    >
+      <div class="flex flex-col space-y-3 sm:space-y-0 sm:flex-row space-x-3">
+        <div class="relative">
+          <button
+            @click="
+              isOpen[0]
+                ? (isOpen[0] = false)
+                : (isOpen = [true, false, false, false])
+            "
+            class="
+              relative
+              z-10
+              block
+              overflow-hidden
+              focus:outline-none focus:border-white
+              ml-3
+            "
+          >
+            Overview
+          </button>
+          <button
+            v-if="isOpen[0]"
+            @click="isOpen[0] = false"
+            tabindex="-1"
+            class="
+              fixed
+              inset-0
+              h-screen
+              w-screen
+              bg-black
+              opacity-50
+              overflow-y-hidden
+              cursor-default
+            "
+          ></button>
+          <div
+            v-if="isOpen[0]"
+            class="fixed mt-2 py-2 z-40 w-48 bg-white rounded-lg shadow-xl"
+          >
+            <a
+              href="#"
+              class="
+                block
+                px-4
+                py-2
+                text-gray-800
+                hover:bg-indigo-500 hover:text-white
+              "
+              >For Creators</a
+            >
+            <a
+              href="#"
+              class="
+                block
+                px-4
+                py-2
+                text-gray-800
+                hover:bg-indigo-500 hover:text-white
+              "
+              >For Patrons</a
+            >
+            <a
+              href="#"
+              class="
+                block
+                px-4
+                py-2
+                text-gray-800
+                hover:bg-indigo-500 hover:text-white
+              "
+              >For Developers</a
+            >
+
+            <a
+              href="#"
+              class="
+                block
+                px-4
+                py-2
+                text-gray-800
+                hover:bg-indigo-500 hover:text-white
+              "
+              >Roadmap</a
+            >
+          </div>
+        </div>
+        <div class="relative">
+          <button
+            @click="
+              isOpen[1]
+                ? (isOpen[1] = false)
+                : (isOpen = [false, true, false, false])
+            "
+            class="
+              relative
+              z-10
+              block
+              overflow-hidden
+              focus:outline-none focus:border-white
+            "
+          >
+            DAO
+          </button>
+          <button
+            v-if="isOpen[1]"
+            @click="isOpen[1] = false"
+            tabindex="-1"
+            class="
+              fixed
+              inset-0
+              h-full
+              w-full
+              bg-black
+              opacity-50
+              cursor-default
+            "
+          ></button>
+          <div
+            v-if="isOpen[1]"
+            class="fixed mt-2 z-40 py-2 w-48 bg-white rounded-lg shadow-xl"
+          >
+            <a
+              href="#"
+              class="
+                block
+                px-4
+                py-2
+                text-gray-800
+                hover:bg-indigo-500 hover:text-white
+              "
+              >Voting Portal</a
+            >
+          </div>
+        </div>
+        <div class="relative">
+          <button
+            @click="
+              isOpen[2]
+                ? (isOpen[2] = false)
+                : (isOpen = [false, false, true, false])
+            "
+            class="
+              relative
+              z-10
+              block
+              overflow-hidden
+              focus:outline-none focus:border-white
+            "
+          >
+            Community
+          </button>
+          <button
+            v-if="isOpen[2]"
+            @click="isOpen[2] = false"
+            tabindex="-1"
+            class="
+              fixed
+              inset-0
+              h-full
+              w-full
+              bg-black
+              opacity-50
+              cursor-default
+            "
+          ></button>
+          <div
+            v-if="isOpen[2]"
+            class="fixed mt-2 z-40 py-2 w-48 bg-white rounded-lg shadow-xl"
+          >
+            <a
+              href="#"
+              class="
+                block
+                px-4
+                py-2
+                text-gray-800
+                hover:bg-indigo-500 hover:text-white
+              "
+              >Forum</a
+            >
+            <a
+              href="#"
+              class="
+                block
+                px-4
+                py-2
+                text-gray-800
+                hover:bg-indigo-500 hover:text-white
+              "
+              >Builders Program</a
+            >
+          </div>
+        </div>
+        <div class="relative">
+          <button
+            @click="
+              isOpen[3]
+                ? (isOpen[3] = false)
+                : (isOpen = [false, false, false, true])
+            "
+            class="
+              relative
+              z-10
+              block
+              overflow-hidden
+              focus:outline-none focus:border-white
+            "
+          >
+            Help
+          </button>
+          <button
+            v-if="isOpen[3]"
+            @click="isOpen[3] = false"
+            tabindex="-1"
+            class="
+              fixed
+              inset-0
+              h-full
+              w-full
+              bg-black
+              opacity-50
+              cursor-default
+            "
+          ></button>
+          <div
+            v-if="isOpen[3]"
+            class="fixed z-40 mt-2 py-2 w-48 bg-white rounded-lg shadow-xl"
+          >
+            <a
+              href="#"
+              class="
+                block
+                px-4
+                py-2
+                text-gray-800
+                hover:bg-indigo-500 hover:text-white
+              "
+              >FAQ</a
+            >
+            <a
+              href="#"
+              class="
+                block
+                px-4
+                py-2
+                text-gray-800
+                hover:bg-indigo-500 hover:text-white
+              "
+              >Tutorials</a
+            >
+            <a
+              href="#"
+              class="
+                block
+                px-4
+                py-2
+                text-gray-800
+                hover:bg-indigo-500 hover:text-white
+              "
+              >Documentation</a
+            >
+
+            <a
+              href="#"
+              class="
+                block
+                px-4
+                py-2
+                text-gray-800
+                hover:bg-indigo-500 hover:text-white
+              "
+              >Code of Conduct</a
+            >
+          </div>
+        </div>
+      </div>
+    </nav>
+    <button class="hidden md:block bg-purple-800 p-2 text-white rounded-md">
+      Launch App
+    </button>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isOpen: [false, false, false, false],
+      smIsOpen: false,
+    };
+  },
+
+  mounted() {
+    const handleEscape = (e) => {
+      if (e.key === "Esc" || e.key === "Escape") {
+        this.isOpen = false;
+      }
+    };
+    document.addEventListener("keydown", handleEscape);
+    this.$once("hook:beforeDestroy", () => {
+      document.removeEventListener("keydown", handleEscape);
+    });
+  },
+};
+</script>
---
+++ src/components/Global/Navigation.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app" class="antialiased text-gray-900">
+    <div>
+      <Navbar />
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from "./Navbar.vue";
+export default {
+  name: "app",
+  components: {
+    Navbar,
+  },
+};
+</script>
