cad5990835537fcb78b336c10f57adb1e38bd3eb
AbhinavMV <mvabhinav1998@gmail.com>
2021-11-17 23:51:08 +0530

    Update Toggle Button

--- src/components/Global/ThemeButton.vue
+++ src/components/Global/ThemeButton.vue
@@ -1,46 +1,72 @@
 <template>
-  <div class="flex justify-end items-center space-x-2 mx-1">
-    <span class="text-sm text-gray-800">Light</span>
-    <div>
-      <input
-        @click="toggleDarkMode"
-        type="checkbox"
-        name=""
-        id="toggle"
-        v-model="isDarkMode"
-        class="hidden"
-      />
-      <label for="toggle">
-        <div
-          class="
-            w-7
-            h-4
-            sm:w-9 sm:h-5
-            flex
-            items-center
-            bg-gray-300
-            rounded-full
-            sm:p-1
-          "
+  <div class="flex items-center mx-4">
+    <button
+      class="
+        w-10
+        h-5
+        md:w-12 md:h-6
+        rounded-2xl
+        bg-white
+        flex
+        items-center
+        transition
+        duration-300
+        focus:outline-none
+        shadow
+      "
+      @click="toggleDarkMode"
+    >
+      <div
+        id="switch-toggle"
+        :class="
+          isDarkMode
+            ? 'bg-gray-700 translate-x-full'
+            : 'bg-yellow-500 -translate-x-2'
+        "
+        class="
+          w-6
+          h-6
+          md:w-7 md:h-7
+          rounded-full
+          transition
+          duration-500
+          transform
+          p-1
+          text-white
+          focus:outline-none
+          select-none
+        "
+      >
+        <svg
+          v-if="!isDarkMode"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
         >
-          <div
-            class="
-              toggle-dot
-              w-3.5
-              h-3.5
-              sm:w-4 sm:h-4
-              bg-white
-              rounded-full
-              shadow-md
-              transform
-              duration-300
-              ease-in-out
-            "
-          ></div>
-        </div>
-      </label>
-    </div>
-    <span class="text-sm text-gray-400">Dark</span>
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+          />
+        </svg>
+        <svg
+          v-if="isDarkMode"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+          />
+        </svg>
+      </div>
+    </button>
   </div>
 </template>
 
