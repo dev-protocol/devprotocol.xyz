d124cf1157bff61fb25c7e9d0aba17974ce4d865
Krish Gupta <krishguptadev@outlook.com>
2021-11-17 21:36:28 +0530

    Apply suggestions from code review
    Co-authored-by: Shubham Kukreti <57281769+KukretiShubham@users.noreply.github.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,21 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./public/**/*.html', "./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}"],
+  purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}'],
   darkMode: 'class',
   theme: {
     extend: {
       colors: {
-        darkSecondary: "#151414"
+        darkSecondary: '#151414'
       },
       fontFamily: {
-        mono: ['IBM Plex Mono', "monospace"],
-        display: ['WhyteInktrap', "sans-serif"],
-        body: ['"Whyte"', "sans-serif"]
+        mono: ['IBM Plex Mono', 'monospace'],
+        display: ['WhyteInktrap', 'sans-serif'],
+        body: ['"Whyte"', 'sans-serif']
       },
       maxWidth: {
         para: '40ch',
-        half: "50%"
+        half: '50%'
       }
-    },
+      }
   },
 }
