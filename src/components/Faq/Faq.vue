b3e411f6839229ee0d0aaba7ef12e691e23d6a14
⧫\_(++)_/⧫ <28738855+Simperfy@users.noreply.github.com>
2021-12-03 12:21:17 +0800

    [FEATURE]: Add categorical view to FAQ (#177)
    * UPDATE: FAQ now viewable via category
    
    * FIX: Mobile view
    
    * UPDATE: Adjusted padding
    
    * FIX: Dropdowns not closing when changing category
    
    * UPDATE: Extended category style's width
    
    * UPDATE: Added missing endline
    
    Co-authored-by: David Leal <halfpacho@gmail.com>
    
    Co-authored-by: Krish Gupta <krishguptadev@outlook.com>
    Co-authored-by: David Leal <halfpacho@gmail.com>

--- src/components/Faq/Faq.vue
+++ src/components/Faq/Faq.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex flex-col justify-center items-center pt-20 pb-10 md:pt-28">
-    <p class="text-center font-display text-4xl">Frequently Asked Questions</p>
+  <div class="flex flex-col justify-center items-center pt-32 pb-10 md:pt-28">
+    <h2 class="text-center mx-auto w-full font-display text-5xl dark:text-white">Frequently Asked Questions</h2>
     <p class="text-center text-sm m-3">
       If you don't find the answer to your questions, please contact us via
       <a href="https://discord.gg/VwJp4KM" class="underline hover:text-blue-600"
@@ -11,7 +11,7 @@
       @click="toggleDropdown"
       class="
         text-xl
-        mt-4
+        mt-10
         select-none
         bg-gray-300
         dark:bg-indigo-600
@@ -20,7 +20,7 @@
         rounded-lg
       "
     >
-      Translate to <span class="underline">{{ selectedLanguage }}</span>
+      Translate to <span class="underline capitalize">{{ selectedLanguage }}</span>
       <img
         src="/assets/dropdown-arrow.svg"
         alt="arrow"
@@ -29,18 +29,45 @@
     </button>
 
     <div
-      :class="isDropdownActive ? 'opacity-100 scale-100 duration-100' : 'invisible absolute opacity-0 scale-95 duration-100'"
-      class="transform transition right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+      :class="isDropdownActive ?
+        'opacity-100 scale-100 duration-100' :
+        'invisible absolute opacity-0 scale-95 duration-100'
+      "
+      class="
+        transform
+        transition
+        right-0
+        mt-2
+        w-56
+        rounded-md
+        shadow-lg
+        bg-white
+        ring-1
+        ring-black
+        ring-opacity-5
+        focus:outline-none
+       "
+    >
       <div class="py-1">
-        <a v-for="(language, index) in languagesArr" :key="index" href="#" @click="translate(language)" class="hover:bg-indigo-600 hover:text-gray-100 text-gray-700 block px-4 py-2 text-sm">{{ language }}</a>
+        <a v-for="(language, index) in languagesArr" :key="index" href="#" @click="translate(language)"
+          class="hover:bg-indigo-600 hover:text-gray-100 text-gray-700 block px-4 py-2 text-sm capitalize">{{ language }}</a>
       </div>
     </div>
+
+    <div class="md:max-w-4xl overflow-x-auto mt-10">
+      <ul class="flex flex-col text-center md:flex-row flex-nowrap mb-2.5">
+        <li v-for="(category, index) in categoriesArr" :key="index" class="pt-2 md:pt-0 md:mr-10 flex-shrink-0 text-xl">
+          <button @click="changeCategory(category)" class="capitalize" :class="selectedCategory === category && 'border-b-4 border-indigo-600'">{{ category }}</button>
+        </li>
+      </ul>
+    </div>
   </div>
+
   <div
-    class="grid md:grid-cols-2 justify-items-stretch md:-mt-20 lg:-mt-40 mb-28"
+    class="flex flex-col justify-center items-center md:-mt-20 lg:-mt-40 mb-28"
   >
-    <div v-for="(item, index) in translatedList" v-bind:key="index" class="my-2">
-      <div class="mx-5 my-4 h-full bg-gray-300 rounded-md dark:bg-indigo-600">
+      <div v-for="(item, index) in translatedList" v-bind:key="item.question + index"
+        class="mx-5 my-4 md:w-6/12 md:max-w-2xl h-full w-11/12">
         <input type="checkbox" :id="'question' + index" class="peer hidden" />
         <div
           class="
@@ -60,16 +87,17 @@
         </div>
         <label
           :for="'question' + index"
-          class="relative text-xl pl-10 p-2 block w-full cursor-pointer"
+          class="relative text-xl pl-10 p-2 block w-full cursor-pointer bg-gray-300  dark:bg-indigo-600 rounded-md"
         >
           {{ item.question }}
         </label>
         <div
           class="
-            peer-checked:pt-3
             tracking-wider
-            peer-checked:h-full peer-checked:opacity-100 peer-checked:px-10
-            !w-full
+            peer-checked:pt-3
+            peer-checked:h-auto
+            peer-checked:opacity-100
+            w-full
             bg-white
             dark:bg-darkPrimary
             h-0
@@ -80,10 +108,10 @@
             ease-in-out
           "
         >
-          {{ item.answer }}
+          <p class="px-10 break-words">{{ item.answer }}</p>
         </div>
       </div>
-    </div>
+
   </div>
 </template>
 
@@ -94,24 +122,109 @@ export default {
   name: "FAQ",
   data() {
     return {
-      questionAndAnswers: [],
-      selectedLanguage: "English",
+      categories: {
+        /*
+          "For Everyone": [
+            {
+              question: {
+                english: "",
+                japanese: ""
+              },
+              answer: {
+                english: "",
+                japanese: ""
+              },
+            },
+            ...
+          ],
+          ...
+        */
+      },
+      selectedCategory: "",
+      selectedLanguage: "english",
+      categoriesArr: [],
       languagesArr: [],
-      isDropdownActive: false
+      isDropdownActive: false,
+      /**
+       * columnHeaderLanguageNameConfig
+       *
+       * Determines how language type is extracted
+       * delimiter - determines where to split the value
+       * substrEnd - determines how many characters to omit from right to left
+       * eg:
+       *  delimiter: "(", substrEnd: 1 - This is will work for "someText(Japanese)"
+       *  delimiter: "---", substrEnd: 0 - This is will work for "someText --- Japanese"
+      */
+      columnHeaderLanguageNameConfig: {
+        delimiter: "(",
+        substrEnd: 1
+      },
     };
   },
   created() {
-    this.questionAndAnswers = langCsv.reduce((acc, langObj) => {
-        const newLanguagesArr = [...Object.entries(langObj).map(([key, value]) => {
-            if (this.languagesArr.indexOf(key) === -1) this.languagesArr.push(key); // get unique languages for our dropdown
-            return { ...JSON.parse(value), language: key };
-        })];
+    langCsv
+    // don't include fields that have empty values
+    .filter((langObj) =>!(Object
+      .values(langObj)
+      .every((value) => value  === '')))
+    // convert to an easy to use object
+    .forEach((langObj) => {
+      const category = langObj.categories.toLowerCase();
 
-        acc.push(...newLanguagesArr);
-        return acc;
-      }, []);
+      // add the first category as default category
+      if (Object.keys(this.categories).length === 0) {
+        this.selectedCategory = category;
+      }
+
+      // store unique category
+      if (this.categoriesArr.indexOf(category) === -1) {
+        this.categoriesArr.push(category);
+      }
+
+      // initialize category
+      if (!this.categories[category]) {
+        this.categories[category] = [];
+      }
+
+      const faqObj = {
+        answer: {},
+        question: {},
+      };
+
+      Object
+      .entries(langObj)
+      .forEach(([key, value]) => {
+        const keyLowerCased = key.toLowerCase();
+
+        if (keyLowerCased.includes('question')) {
+          const languageName = this.extractLanguageName(keyLowerCased);
+          faqObj.question[languageName] = value.trim();
+        }
+
+        if (keyLowerCased.includes('answer')) {
+          const languageName = this.extractLanguageName(keyLowerCased);
+          faqObj.answer[languageName] = value.trim();
+        }
+      });
+
+      this.categories[category].push(faqObj);
+    });
   },
   methods: {
+    changeCategory: function(category) {
+      this.selectedCategory = category;
+    },
+    storeUniqueLanguageName: function(languageName) {
+      if (this.languagesArr.indexOf(languageName) === -1) this.languagesArr.push(languageName);
+    },
+    extractLanguageName: function(keyLowerCased) {
+      let languageName = keyLowerCased.split(this.columnHeaderLanguageNameConfig.delimiter)[1].trim();
+      languageName = languageName.substring(0, languageName.length - this.columnHeaderLanguageNameConfig.substrEnd);
+
+      this.storeUniqueLanguageName(languageName);
+
+      return languageName;
+    },
     translate: function (selectedLanguage) {
       this.selectedLanguage = selectedLanguage;
       this.toggleDropdown();
@@ -122,7 +235,8 @@ export default {
   },
   computed: {
     translatedList: function () {
-      return this.questionAndAnswers.filter((item) => item.language === this.selectedLanguage);
+      return this.categories[this.selectedCategory]
+        .map((faqItem) => ({question: faqItem.question[this.selectedLanguage], answer: faqItem.answer[this.selectedLanguage]}));
     },
   },
 };
