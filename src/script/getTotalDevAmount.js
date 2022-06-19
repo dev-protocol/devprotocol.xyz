584f02dbbf74ac7f4067194d976d95c79fbb9e86
osaguild <nobuhisa.abe.0922@gmail.com>
2022-06-19 23:01:22 +0900

    fix script

--- src/script/getTotalDevAmount.js
+++ src/script/getTotalDevAmount.js
@@ -1,36 +1,22 @@
 const BigNumber = require('bignumber.js')
+const R = require('ramda')
 const fs = require('fs')
 
-const chains = [
-  {
-    name: 'mainnet',
-    endpoint: 'https://api.thegraph.com/subgraphs/name/dev-protocol/mainnet-v2',
-  },
-  {
-    name: 'arbitrum',
-    endpoint:
-      'https://api.thegraph.com/subgraphs/name/dev-protocol/arbitrum-one-v2',
-  },
-  {
-    name: 'polygon',
-    endpoint: 'https://api.thegraph.com/subgraphs/name/dev-protocol/polygon-v2',
-  },
-]
-
-const fetchAll = async (chains) =>
+// Define `fetchAll` function to fetch all data from TheGraph
+const fetchAll = async (endpoint) =>
   new Promise((resolve) => {
     let data = []
     const f = async (timestamp = 0) => {
-      const res = await fetch(chains.endpoint, {
+      const res = await fetch(endpoint, {
         method: 'POST',
         body: JSON.stringify({
           query: `{
-                    totalLockedAmounts(first: 1000, orderBy: timestamp, orderDirection: asc, where:{timestamp_gt: ${timestamp}}) {
-                        id
-                        timestamp
-                        amount
-                    }
-                }`,
+              totalLockedAmounts(first: 1000, orderBy: timestamp, orderDirection: asc, where:{timestamp_gt: ${timestamp}}) {
+                  id
+                  timestamp
+                  amount
+              }
+          }`,
         }),
       })
       const json = await res.json()
@@ -38,7 +24,6 @@ const fetchAll = async (chains) =>
         console.log('respond', json.data.totalLockedAmounts.length)
         const items = json.data.totalLockedAmounts.map(
           ({ timestamp, amount, id }) => ({
-            chain: chains.name,
             id,
             timestamp,
             amount: new BigNumber(amount).div(1e18).toNumber(),
@@ -47,19 +32,75 @@ const fetchAll = async (chains) =>
         data = [...data, ...items]
         f(items[items.length - 1].timestamp)
       } else {
-        console.log('finish', chains.name)
+        console.log('finish', endpoint)
         resolve(data)
       }
     }
     f(0)
   })
 
+const dateToKey = (date) => {
+  return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDay() + 1}`
+}
+
+const byDay = R.groupBy((item) => {
+  const { timestamp } = item
+  const date = new Date(Number(timestamp) * 1000)
+  return dateToKey(date)
+})
+
+const diff = (a, b) => a - b
+
+const pickLasts = (items) =>
+  R.keys(items).map((key) => {
+    const dayItems = R.sort(diff, items[key])
+    return {
+      key,
+      amount: R.last(dayItems).amount,
+    }
+  })
+
 // Run the graphing
 ;(async () => {
-  const promises = chains.map(fetchAll)
+  const promises = [
+    'https://api.thegraph.com/subgraphs/name/dev-protocol/mainnet-v2',
+    'https://api.thegraph.com/subgraphs/name/dev-protocol/arbitrum-one-v2',
+    'https://api.thegraph.com/subgraphs/name/dev-protocol/polygon-v2',
+  ].map(fetchAll)
   const [mainnet, arb, polygon] = await Promise.allSettled(promises)
+
+  const picked = [
+    pickLasts(byDay(mainnet.value)),
+    pickLasts(byDay(arb.value)),
+    pickLasts(byDay(polygon.value)),
+  ]
+  const labels = R.sort(
+    diff,
+    picked.flat().map(({ key }) => new Date(key))
+  ).map(dateToKey)
+
+  const [m, a, p] = picked.map((chain) => {
+    let lastValue = 0
+    return labels.map((key) =>
+      ((data) => {
+        if (data) {
+          lastValue = data.amount
+        }
+        return data ? data.amount : lastValue
+      })(chain.find(({ key: K }) => key === K))
+    )
+  })
+  const graphData = []
+  for (let i = 0; i < labels.length; i++) {
+    graphData.push({
+      date: labels[i],
+      mainnet: m[i],
+      arb: a[i],
+      polygon: p[i],
+    })
+  }
   fs.writeFileSync(
     './src/script/data/data.json',
-    JSON.stringify(mainnet.value.concat(arb.value, polygon.value), null, '  ')
+    JSON.stringify(graphData, null, '  ')
   )
 })()
